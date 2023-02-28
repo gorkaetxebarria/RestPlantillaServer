ac1d12b93cdea2c3b78379a26c945a22eff23ea8
gorka etxebarria <gorka.etxebarria@gmail.com>
2023-02-28 15:26:56 +0100

    add cliente

---
+++ plantillaCliente/src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.net.URI;
+
+import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import jakarta.ws.rs.core.UriBuilder;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Starting Embedded Jersey HTTPServer...\n");
+        ResourceConfig resourceConfig = new ResourceConfig();
+        resourceConfig.packages("controller");
+        URI uri = UriBuilder.fromUri("http://localhost/").port(9090).build();
+
+        JdkHttpServerFactory.createHttpServer(uri, resourceConfig);
+    }
+}
